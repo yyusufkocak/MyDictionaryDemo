2b257827a35393be4318fcdef0e376c06b2c1280
xatlive <39595475+xatlive@users.noreply.github.com>
2021-03-08 19:23:25 +0300

    Add project files.

---
+++ MyDictionaryDemo/MyDictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryDemo
+{
+    class MyDictionary<K,V>
+    {
+        K[] keys;
+        V[] values;
+
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+
+
+         public void Add(K key,V value)
+        {
+            K[] tempArrayK = keys;
+            keys = new K[keys.Length + 1];
+
+            V[] tempArrayV = values;
+            values = new V[values.Length + 1];
+
+
+
+            for (int i = 0; i <tempArrayK.Length; i++)
+            {
+                keys[i]=tempArrayK[i];
+                values[i] = tempArrayV[i];
+
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+        }
+        
+        public int Count
+        {
+            get { return keys.Length; }
+        }
+
+
+
+    }
+}
---
+++ MyDictionaryDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1727,"Yusuf KOÇAK");
+            myDictionary.Add(1728, "Melike KOÇAK");
+            myDictionary.Add(1729, "Mesut KOÇAK");
+            myDictionary.Add(1730, "Özlem KOÇAK");
+            Console.WriteLine(myDictionary.Count);
+
+
+        }
+    }
+}
